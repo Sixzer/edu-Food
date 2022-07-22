e6107d882a4dd1b32ec1adecbc53ba4d4e6a0835
Sixzer <iam6zer@gmail.com>
2022-07-22 19:36:10 +0300

    add async/await to fetch

--- js/script.js
+++ js/script.js
@@ -150,12 +150,7 @@ window.addEventListener('DOMContentLoaded', function() {
             this.price = price;
             this.classes = classes;
             this.parent = document.querySelector(parentSelector);
-            this.transfer = 27;
-            this.changeToUAH(); 
-        }
-
-        changeToUAH() {
-            this.price = this.price * this.transfer; 
+            this.transfer = 27; 
         }
 
         render() {
@@ -175,40 +170,55 @@ window.addEventListener('DOMContentLoaded', function() {
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    <div class="menu__item-total"><span>${this.price}</span> USDT/день</div>
                 </div>
             `;
             this.parent.append(element);
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container"
-    ).render();
+    const getResource = async(url) => {
+        const result = await fetch(url);
+         
+        if (!result.ok) {
+            throw new Error(`Coudnt fetch ${url}, status: ${result.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        14,
-        ".menu .container"
-    ).render();
- 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        21,
-        ".menu .container"
-    ).render();
+        return await result.json();
+    }
 
+    // 1 WAY
+    getResource("http://localhost:3000/menu")
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+    });
+
+    //2 WAY
+    // getResource("http://localhost:3000/menu")
+    //     .then(data => createCard(data));
+
+    // const createCard = function(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const el = document.createElement("div");
+
+    //         el.classList.add("menu__item");
+
+    //         el.innerHTML = `
+    //         <img src=${img} alt=${altimg}>
+    //         <h3 class="menu__item-subtitle">${title}</h3>
+    //         <div class="menu__item-descr">${descr}</div>
+    //         <div class="menu__item-divider"></div>
+    //         <div class="menu__item-price">
+    //             <div class="menu__item-cost">Цена:</div>
+    //             <div class="menu__item-total"><span>${price}</span> USDT/day</div>
+    //         </div>
+    //     `;
+
+    //     document.querySelector(".menu .container").append(el);
+    //     });
+    // }
     //Forms
 
     const forms = document.querySelectorAll("form");
@@ -219,9 +229,19 @@ window.addEventListener('DOMContentLoaded', function() {
         failure: "Smth went wrong",
     };
 
+    const postData = async(url, data) => {
+        const result = await fetch(url, {
+            method: "POST",
+                headers: {
+                    "Content-type": "application/JSON"
+                },
+                body: data
 
+        });
+        return await result.json();
+    }
 
-    const postData = function(form) {
+    const bindPostData = function(form) {
         form.addEventListener("submit",(e) => {
             e.preventDefault();
 
@@ -235,19 +255,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
             const formData = new FormData(form);
 
-            const obj = {};
-            formData.forEach(function(value,key) {   
-                obj[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch("server.php",{
-                method: "POST",
-                headers: {
-                    "Content-type": "application/JSON"
-                },
-                body: JSON.stringify(obj)
-            })
-            .then(data => data.text())
+            postData("http://localhost:3000/requests", json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -262,7 +272,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
     const showThanksModal = function(message) {
@@ -288,7 +298,6 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal(); 
         }, 4000);
     }
-
 });
 
 
